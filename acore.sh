01812ff6fd69eba8e25f9348723f4f9e0debdc22
jiangmeng0606 <jiangmeng0606@qq.com>
2023-08-20 21:19:25 +0800

    add 个人配置

--- acore.sh
+++ acore.sh
@@ -4,5 +4,11 @@
 
 CUR_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+export no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com"
+export http_proxy="http://jiangmeng0606.tpddns.cn:8868"
+export https_proxy=$http_proxy
+export all_proxy="socks5://jiangmeng0606.tpddns.cn:8668"
+echo -e "已开启代理"
+
 
 source "$CUR_PATH/apps/installer/main.sh"
